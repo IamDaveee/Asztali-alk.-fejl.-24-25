ec52cdece27dad29e9cf4f30a6841a38f5b6bb0e
Dave <121976848+IamDaveee@users.noreply.github.com>
2025-03-11 13:52:39 +0100

    Add files via upload

---
+++ Snooker_03.11/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Snooker_03._11
+{
+    class Program
+    {
+        static List<adat> Lista = new List<adat>();
+        struct adat
+        {
+            public int helyezes, nyeremeny;
+            public string nev, orszag;
+        }
+
+        static void Feladat2()
+        {
+            StreamReader sr = new StreamReader("snooker.txt", Encoding.Default);
+            string elsoSor = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] egysor = sr.ReadLine().Split(';');
+                adat temp = new adat();
+                temp.helyezes = Convert.ToInt32(egysor[0]);
+                temp.nev = egysor[1];
+                temp.orszag = egysor[2];
+                temp.nyeremeny = Convert.ToInt32(egysor[3]);
+                Lista.Add(temp);
+            }
+            sr.Close();
+        }
+        static void Feladat3()
+        {
+            Console.WriteLine($"A világranglistában {Lista.Count} versenyző szerepel");
+        }
+        static void Feladat4()
+        {
+            double sum = 0;
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                sum += Lista[i].nyeremeny;
+            }
+            Console.WriteLine($"A versenyzők átlagosan {sum/Lista.Count:0.00} nyereményre tettek szert");
+        }
+
+        static double Feladat5()
+        {
+            double sum = 0;
+            int count = 0;
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].orszag=="Anglia")
+                {
+                    sum += Lista[i].nyeremeny;
+                    count++;
+                }
+            }
+            double avg =sum/ count;
+            return avg;
+        }
+
+        static void Feladat6()
+        {
+            int max = 0;
+            int maxi = 0;
+
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].orszag=="Kína")
+                {
+                    if (Lista[i].nyeremeny>max)
+                    {
+                        max = Lista[i].nyeremeny;
+                        maxi = i;
+                    }
+                }
+            }
+            Console.WriteLine($"A legjobban kereső Kínai versenyző: \n\tNév: {Lista[maxi].nev}\n\tHelyezés: {Lista[maxi].helyezes}\n\tNyeremény: {Lista[maxi].nyeremeny*380} Ft");
+        }
+
+        static bool Feladat7()
+        {
+            bool van = false;
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].orszag=="Norvégia")
+                {
+                    van = true;
+                }
+            }
+            return van;
+        }
+
+        static void Feladat8()
+        {
+            StreamWriter sw = new StreamWriter("anglia.txt");
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (Lista[i].orszag=="Anglia")
+                {
+                    sw.WriteLine($"{Lista[i].nev}\t{Lista[i].helyezes}\t{Lista[i].nyeremeny}");
+                }
+            }
+            sw.Close();
+        }
+        
+        static void Main(string[] args)
+        {
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Console.WriteLine($"Az Angliai versenyzők átlagosan {Feladat5():0.00} bevételre tettek szert");
+            Feladat6();
+            Console.WriteLine(Feladat7()?$"A top 100 ban van Norvég játékos":$"A top 100 ban nincs Norvég játékos");
+            Feladat8();
+        }
+    }
+}
---
+++ Structure_gyak_03.11/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Structure_gyak_03._11
+{
+    class Program
+    {
+        //saját típus - struktúra létrehozása
+        struct adat
+        {
+            public string varos, nev1, nev2;
+            public double ferohely;
+        }
+
+        //összetett adatszerkezet - struktúra típusú lista
+        static List<adat> adatok = new List<adat>();
+        static void Beolvas()
+        {
+            Console.WriteLine("1.feladat:\tAz adatok beolvasása: ");
+            StreamReader sr = new StreamReader("vb2018.txt", Encoding.Default);
+
+            //első sor kezelése
+            string elsoSor = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string[] egysor = sr.ReadLine().Split(';');
+                adat segedAdat = new adat();
+                segedAdat.varos = egysor[0];
+                segedAdat.nev1 = egysor[1];
+                segedAdat.nev2 = egysor[2];
+                segedAdat.ferohely = Convert.ToDouble(egysor[3]);
+                adatok.Add(segedAdat);
+            }
+            sr.Close();
+        }
+
+        static void Kiir()
+        {
+            Console.WriteLine("2. feladat: \ta beolvasott adatok:");
+            Console.WriteLine("\tvaros\tnév1\tnév2\tférőhely");
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                Console.WriteLine($"\t{adatok[i].varos} {adatok[i].nev1} {adatok[i].nev2} {adatok[i].ferohely}");
+            }
+        }
+
+        static void Legkevesebb()
+        {
+            double min = adatok[0].ferohely;
+            int mini = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].ferohely<min)
+                {
+                    min = adatok[i].ferohely;
+                    mini = i;
+                }
+            }
+            Console.WriteLine($"4. feladat: A legkevesebb férőhely: ");
+            Console.WriteLine($"\tVáros: {adatok[mini].varos}");
+            Console.WriteLine($"\tStadion neve: {adatok[mini].nev1}");
+            Console.WriteLine($"\tFérőhely: {adatok[mini].ferohely}");
+        }
+
+        static double Atlag()
+        {
+            double osszes= 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                osszes += adatok[i].ferohely;
+            }
+            return osszes / adatok.Count;
+        }
+
+        static double Moszkva()
+        {
+            double osszes = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].varos=="Moszkva")
+                {
+                    osszes += adatok[i].ferohely;
+                }
+            }
+            return osszes;
+        }
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Kiir();
+            Console.WriteLine($"3. feladat:\n\tStadionok száma: {adatok.Count}");
+            Legkevesebb();
+            Console.WriteLine($"\n5. feladat: Átlagos férőhelyek száma: {Atlag():0.00}");
+            Console.WriteLine($"\n6. feladat: A moszkvai stadionok összesen {Moszkva()} főt");
+        }
+    }
+}
