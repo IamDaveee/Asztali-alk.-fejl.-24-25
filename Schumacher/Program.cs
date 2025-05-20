0ec08835054ab1b5d3a0871af2e921a5e979d5fb
Dave <121976848+IamDaveee@users.noreply.github.com>
2025-05-20 10:57:22 +0200

    Add files via upload

---
+++ Schumacher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //példány - az autómatikusan elészített konstruktor használatára
+            Schumi példány = new Schumi(new DateTime(2025, 05, 13),"Magyar Nagydíj","11G","Vesztes",100,0,0);
+
+            Console.WriteLine(példány.dátum.Year);
+
+            //lista készítése
+            List<Schumi> adatok = new List<Schumi>();
+
+            //beolvasás
+            System.IO.File.ReadAllLines("schumacher.csv").Skip(1).ToList().ForEach(x => adatok.Add(new Schumi(x)));
+            Console.WriteLine($"3. fealadat: {adatok.Count} sort tartalmaz");
+
+            Console.WriteLine($"4.feladat: {adatok.Where(x=>x.nagydijNeve=="Hungarian Grand Prix"&& x.statusz=="Finished").Count()}");
+            Console.WriteLine("Sikeres szereplései");
+            
+            adatok.Where(x => x.nagydijNeve == "Hungarian Grand Prix" && x.helyezes > 0).ToList().ForEach(x => Console.WriteLine($"{x.dátum.Year}.{x.dátum.Month:00}.{x.dátum.Day:00} - {x.helyezes}.hely"));
+            
+            adatok.Where(x => x.statusz != "Finished").GroupBy(x => x.statusz).Where(y => y.Count() > 2).ToList().ForEach(y => Console.WriteLine($"{y.Key} - előfordulása: {y.Count()}"));
+
+
+        }
+    }
+}
---
+++ Schumacher/Schumi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumacher
+{
+    class Schumi
+    {
+        //adattagok - láthstóság, hozzáférés: public, private
+        public DateTime dátum;
+        public string nagydijNeve, csapat, statusz;
+        public int helyezes, befejezettKorok, szerzettPontok;
+
+        public Schumi(DateTime dátum, string nagydijNeve, string csapat, string statusz, int helyezes, int befejezettKorok, int szerzettPontok)
+        {
+            this.dátum =Convert.ToDateTime(dátum);
+            this.nagydijNeve = nagydijNeve;
+            this.csapat = csapat;
+            this.statusz = statusz;
+            this.helyezes = helyezes;
+            this.befejezettKorok = befejezettKorok;
+            this.szerzettPontok = szerzettPontok;
+        }
+
+
+        //konstruktor - fájlbeolvasást
+        public Schumi(string sor)
+        {
+            string[] egysor = sor.Split(';');
+            dátum = Convert.ToDateTime(egysor[0]);
+            nagydijNeve = egysor[1];
+            helyezes = Convert.ToInt32(egysor[2]);
+            befejezettKorok = Convert.ToInt32(egysor[3]);
+            szerzettPontok = Convert.ToInt32(egysor[4]);
+            csapat = egysor[5];
+            statusz= egysor[6];
+        }
+
+
+        //metódus - eljárások, függvények
+
+    }
+}
